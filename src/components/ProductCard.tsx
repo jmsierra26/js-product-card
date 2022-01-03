a281cdff6fda244a5a8851d1853d105a679489f6
Jesús Sierra <dev.jesus.sierra@gmail.com>
2022-01-03 13:28:22 -0500

    Versión 1.0.0-rc lista.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -19,8 +19,7 @@ export interface Props {
 }
 
 export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
-  const { counter, increaseBy, maxCount, isMaxCountReached, reset } =
-    useProduct({ product, onChange, value, initialValues })
+  const { counter, increaseBy, maxCount, isMaxCountReached, reset } = useProduct({ product, onChange, value, initialValues })
 
   return (
     <Provider value={{
